62698e0e50a33bcc0c27a68d295a7789a83f3432
Enrico B <rflnrclws@protonmail.com>
2017-06-07 14:28:15 -0400

    Get basic front-end up

---
+++ content_scripts/test.js
@@ -0,0 +1,14 @@
+function beastify(request, sender, sendResponse) {
+  removeEverything();
+  insertBeast(request.beastURL);
+  browser.runtime.onMessage.removeListener(beastify);
+}
+
+/*
+Remove every node under document.body
+*/
+function removeEverything() {
+  while (document.body.firstChild) {
+    document.body.firstChild.remove();
+  }
+}
