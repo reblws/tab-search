37fad7ba5332457d773e37aec1edeae908037122
Enrico B <rflnrclws@protonmail.com>
2017-07-03 11:13:58 -0400

    Finish focus navigation with new tab-objects

--- popup/tab_search.js
+++ popup/tab_search.js
@@ -6,7 +6,7 @@ const tabList = document.querySelector('.tab-list');
   getAllTabs().then(injectTabsInList);
 }
 // Set a timeout on focus so input is focused on popup
-setTimeout(() => searchInput.focus(), 100);
+setTimeout(() => window.focus(), 100);
 // Any keydown should activate the search field,
 // TODO: on arrow keys ignore focus and switch tab index
 window.addEventListener('keydown', handleKeyDown);
@@ -15,13 +15,14 @@ searchInput.addEventListener('change', updateSearch);
 searchInput.addEventListener('keyup', updateSearch);
 
 function handleKeyDown(event) {
+  console.log('key');
   switch (event.key) {
     case "Tab":
+    case "ArrowDown":
+    case "ArrowUp":
       event.preventDefault();
     case "ArrowRight":
-    case "ArrowDown":
     case "ArrowLeft":
-    case "ArrowUp":
       navigateResults(event.key);
       // document.activeElement.previousElementSibling.focus();
       break;
@@ -29,7 +30,7 @@ function handleKeyDown(event) {
       if (document.activeElement.nodeName === "INPUT") {
         document.querySelector('.tab-object').focus();
       }
-      switchTabs(document.activeElement.dataset.id);
+      switchActiveTab(document.activeElement.dataset.id);
       break;
     default:
       searchInput.focus();
@@ -38,8 +39,8 @@ function handleKeyDown(event) {
 }
 
 function navigateResults(direction) {
-  if (document.activeElement.nodeName !== 'A') {
-    document.querySelector('.tab-link').focus();
+  if (![...document.activeElement.classList].includes('tab-object')) {
+    document.querySelector('.tab-object').focus();
     return;
   }
 
@@ -48,11 +49,17 @@ function navigateResults(direction) {
     case "ArrowRight":
     case "ArrowDown":
       const nextSibling = document.activeElement.nextElementSibling;
-      if (nextSibling) nextSibling.focus();
+      if (nextSibling) {
+        nextSibling.focus();
+      } else {
+        // Return to top if next !exist
+        tabList.querySelector('.tab-object').focus();
+      }
       break;
     case "ArrowLeft":
     case "ArrowUp":
       const prevSibling = document.activeElement.previousElementSibling;
+      // TODO: return to top on else
       if (prevSibling) prevSibling.focus();
       break;
   }
@@ -81,9 +88,8 @@ function injectTabsInList(tabArray) {
 }
 
 function tabToTag(tab, index) {
-  const className = 'tab-link';
   return `
-    <div class="tab-object" data-id="${tab.id}">
+    <div class="tab-object" data-id="${tab.id}" tabIndex="0">
       <img src="${tab.favIconUrl}">
       <div class="tab-info">
         <strong>${tab.title}</strong>
@@ -96,12 +102,12 @@ function tabToTag(tab, index) {
 function switchTabs(event) {
   if (event.target.className !== 'tab-object') return;
   event.preventDefault();
-  const tabId = parseInt(event.target.dataset.id);
   switchActiveTab(tabId);
 }
 
 function switchActiveTab(id) {
-  browser.tabs.update(id, { active: true });
+  const numId = parseInt(id);
+  browser.tabs.update(numId, { active: true });
   window.close();
 }
 
