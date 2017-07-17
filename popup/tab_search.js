cbbce1a7c5f5844338900acf890f43411bf2cfe0
Enrico B <rflnrclws@protonmail.com>
2017-07-17 19:01:15 -0400

    Add todo

--- popup/tab_search.js
+++ popup/tab_search.js
@@ -291,6 +291,15 @@ function updateSearch(event) {
 
 function filterResults(query) {
   const searchUrl = queryInUrl(query);
+
+  // TODO: Still need to do a strict match for url
+  //  Should: Do a filter over strict url maatches
+  //  THEN: they should jump to the top of the results,
+  //      wrt to their same rankings relative rank to each
+  //      other in the original Fuse search.
+  //          -> Loop thru the fuse results,
+  //              no loops needed, just filter both arrays,
+  //              join them (filteredFuse first),
   const options = {
     shouldSort: true,
     // includeMatches: false,
