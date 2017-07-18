0f2f766e46f2b5e45ba2d0c16db33c7d619f3152
Enrico B <rflnrclws@protonmail.com>
2017-07-18 16:36:13 -0400

    Tweak search threshold to be lower and handle url matches

--- src/search.js
+++ src/search.js
@@ -2,25 +2,27 @@ import Fuse from 'fuse.js';
 
 export default function filterResults(query) {
   const searchUrl = queryInUrl(query);
-
-  // TODO: Still need to do a strict match for url
-  //  Should: Do a filter over strict url maatches
-  //  THEN: they should jump to the top of the results,
-  //      wrt to their same rankings relative rank to each
-  //      other in the original Fuse search.
-  //          -> Loop thru the fuse results,
-  //              no loops needed, just filter both arrays,
-  //              join them (filteredFuse first),
   const options = {
     shouldSort: true,
     // includeMatches: false,
-    threshold: 0.7,
+    threshold: 0.5,
     maxPatternLength: 32,
     minMatchCharLength: 1,
     keys: ['title'],
   };
   const fuse = array => new Fuse(array, options);
-  return tabArray => fuse(tabArray).search(query);
+  return (tabArray) => {
+    const fuseResults = fuse(tabArray).search(query);
+    const urlResults = tabArray.filter(searchUrl);
+    const headUrlMatches = fuseResults.filter(tab => urlResults.includes(tab));
+    const tailUrlMatches = urlResults.filter(tab => !fuseResults.includes(tab));
+    const tailMatches = fuseResults.filter(tab => !headUrlMatches.includes(tab));
+    return [
+      ...headUrlMatches,
+      ...tailUrlMatches,
+      ...tailMatches,
+    ];
+  };
 }
 
 function queryInUrl(query) {
