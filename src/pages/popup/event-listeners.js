dbbd051eacb0b1d4fb46f5f70aaab4c53ca991cb
Enrico B <rflnrclws@protonmail.com>
2017-10-12 16:26:35 -0400

    Add ability to delete tabs with CTRL+Click

--- src/pages/popup/event-listeners.js
+++ src/pages/popup/event-listeners.js
@@ -3,14 +3,14 @@ import {
   deleteButton,
 } from './constants';
 import {
-  switchTabs,
   scrollIfNeeded,
 } from './dom-utils';
 import {
   handleKeyDown,
   configureSearch,
   clearInput,
-} from './event-handlers';
+  handleTabClick,
+} from './event-callbacks';
 
 export function addEventListeners(store) {
   const updateSearchResults = configureSearch(store);
@@ -26,6 +26,6 @@ export function addEventListeners(store) {
 }
 
 export function addTabListeners(tabNode) {
-  tabNode.addEventListener('click', switchTabs, true);
+  tabNode.addEventListener('click', handleTabClick, true);
   tabNode.addEventListener('focus', scrollIfNeeded);
 }
