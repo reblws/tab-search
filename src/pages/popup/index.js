c4e32b5fe362f3f7227c9f23a0bee011bce77095
Enrico B <rflnrclws@protonmail.com>
2017-10-12 15:27:45 -0400

    Add currentWindowId to store object

--- src/pages/popup/index.js
+++ src/pages/popup/index.js
@@ -3,19 +3,19 @@ import { createUIStore } from 'redux-webext';
 import addEventListeners from './event-listeners';
 import { populateTabList } from './dom-utils';
 
+
 // Add a key containing adding the current windowId to the popup.
 const windowIdPromise = browser.windows.getCurrent()
-  .then(({ id }) => ({ windowId: id }));
+  .then(({ id }) => ({ currentWindowId: id }));
 // Adds a key 'windowId' to the store object.
-// Store object is now -> { dispatch, getState, subscribe, windowId }
-const mergeStoreWithWindowId = promiseArray =>
-  promiseArray.reduce((acc, obj) => Object.assign({}, acc, obj));
+// Store object is now -> { dispatch, getState, subscribe, currentWindowId }
+const mergeObjects = arr => arr.reduce((acc, obj) => Object.assign({}, acc, obj));
 
-Promise.all([createUIStore(), windowIdPromise])
-  .then(mergeStoreWithWindowId)
+Promise.all([createUIStore(), windowIdPromise]).then(mergeObjects)
   .then(populateTabList)
   .then(addEventListeners)
   .catch((e) => {
     console.error(e);
     throw new Error('Ran into a problem initializing popup window.');
   });
+
