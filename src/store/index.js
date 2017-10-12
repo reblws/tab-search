9fba05d528e6667e5488a2e98e809411662b2548
Enrico B <rflnrclws@protonmail.com>
2017-10-12 15:28:47 -0400

    Persist settings in localStorage with redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import rootReducer from '../reducers';
 
-export default createStore(
+const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, logger),
+  compose(
+    applyMiddleware(thunkMiddleware, logger),
+    autoRehydrate(),
+  ),
 );
+
+persistStore(store, {
+  whitelist: ['settings'],
+});
+
+export default store;
