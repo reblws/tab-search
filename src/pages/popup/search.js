8294a6ee810eba321b5dadb5290f5723faf71132
Enrico B <rflnrclws@protonmail.com>
2017-10-12 15:28:08 -0400

    Use multi-key search in fuse

--- src/pages/popup/search.js
+++ src/pages/popup/search.js
@@ -1,30 +1,13 @@
 import Fuse from 'fuse.js';
 
 export default function filterResults(query) {
-  const searchUrl = queryInUrl(query);
   const options = {
     shouldSort: true,
     // includeMatches: false,
     threshold: 0.5,
     maxPatternLength: 32,
     minMatchCharLength: 1,
-    keys: ['title'],
+    keys: ['title', 'url'],
   };
-  const fuse = array => new Fuse(array, options);
-  return (tabArray) => {
-    const fuseResults = fuse(tabArray).search(query);
-    const urlResults = tabArray.filter(searchUrl);
-    const headUrlMatches = fuseResults.filter(tab => urlResults.includes(tab));
-    const tailUrlMatches = urlResults.filter(tab => !fuseResults.includes(tab));
-    const tailMatches = fuseResults.filter(tab => !headUrlMatches.includes(tab));
-    return [
-      ...headUrlMatches,
-      ...tailUrlMatches,
-      ...tailMatches,
-    ];
-  };
-}
-
-function queryInUrl(query) {
-  return ({ url }) => url.toLowerCase().includes(query);
+  return tabs => new Fuse(tabs, options).search(query);
 }
