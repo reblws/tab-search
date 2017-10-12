a355f89e1a6c31a3ec2d39416266d68b07b6628f
Enrico B <rflnrclws@protonmail.com>
2017-10-12 15:36:43 -0400

    Remove comment

--- src/pages/background/store.js
+++ src/pages/background/store.js
@@ -2,6 +2,3 @@ import { createBackgroundStore } from 'redux-webext';
 import store from '../../store';
 
 export default createBackgroundStore({ store });
-
-// Instead of initializing tabs on browser action, set event listeners here to
-// dispatch
