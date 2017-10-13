0b7cb588baf66fa5f90096d0a778f235e99fe213
Enrico B <rflnrclws@protonmail.com>
2017-10-13 16:28:09 -0400

    Clean up webpack config and add new package.json scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,124 +1,37 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const { join } = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production';
-// Keep this constant because CopyWebPack changes dirs
-const DIRNAME = __dirname;
-const distFolder = path.join(DIRNAME, 'dist');
-const releaseFolder = browser => path.join(DIRNAME, 'releases', browser);
+const srcPath = join(__dirname, 'src');
+const distPath = join(__dirname, 'dist');
+const distAssetsJsPath = join(distPath, 'assets', 'js');
+const pagesPath = join(srcPath, 'pages');
 
-function getPlugins() {
-  const plugins = [];
-  if (!isProduction) return plugins;
-  plugins.push(new CleanWebpackPlugin(['releases']));
-  const copyWebPack = new CopyWebpackPlugin([
-    // Firefox
-    {
-      from: path.join(distFolder, 'manifest.json'),
-      to: releaseFolder('firefox'),
-    },
-    {
-      from: path.join(distFolder, 'assets', '*.svg'),
-      to: path.join(releaseFolder('firefox'), 'assets'),
-      flatten: true,
-    },
-    {
-      from: path.join(distFolder, 'popup'),
-      to: path.join(releaseFolder('firefox'), 'popup'),
-    },
-    {
-      from: path.join(distFolder, 'icons'),
-      to: path.join(releaseFolder('firefox'), 'icons'),
-    },
-    // Chrome
-    {
-      from: path.join(distFolder, 'assets'),
-      to: path.join(releaseFolder('chrome'), 'assets'),
-    },
-    {
-      from: path.join(distFolder, 'popup'),
-      to: path.join(releaseFolder('chrome'), 'popup'),
-    },
-    {
-      from: path.join(distFolder, 'icons'),
-      to: path.join(releaseFolder('chrome'), 'icons'),
-    },
-    {
-      from: path.join(distFolder, 'manifest.json'),
-      to: releaseFolder('chrome'),
-      // Have to transform the file to chrome-compatible manifest
-      transform: transformToChromeManifest,
-    },
-  ], {
-    copyUnmodified: true,
-  });
-
-  plugins.push(copyWebPack);
-
-  return plugins;
-}
-
-// Transform the firefox manifest into one compatible with chrome
-function transformToChromeManifest(content) {
-  /*
-  * Keys to modify:
-  *    -- browser_action.default_icon: "assets/search64.png"
-  * Keys to add:
-  *    -- minimum_chrome_version: "59.0.3071"
-  * Keys to remove:
-  *    -- applications
-  */
-  const manifest = JSON.parse(content);
-  delete manifest.applications;
-
-  manifest.browser_action.default_icon = 'icons/logo-48.png';
-  manifest.minimum_chrome_version = '59.0.3071';
-  return JSON.stringify(manifest, null, 2);
-}
-
-function getOutput(filename) {
-  const output = {};
-  if (isProduction) {
-    output.path = path.join(__dirname, 'releases', 'firefox', 'popup');
-    output.filename = filename;
-  } else {
-    output.path = path.join(__dirname, 'dist', 'popup');
-    output.filename = filename;
-  }
-  return output;
-}
-
-function getLoaders() {
-  return [
-    {
-      test: /\.js$/i,
-      loader: 'babel-loader',
-      query: {
-        plugins: ['transform-es2015-modules-commonjs'],
+module.exports = ({ targetBrowser }) => ({
+  entry: {
+    popup: join(pagesPath, 'popup', 'index.js'),
+    background: join(pagesPath, 'background', 'index.js'),
+    settings: join(pagesPath, 'settings', 'index.js'),
+  },
+  output: {
+    path: distAssetsJsPath,
+    filename: '[name]_bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/i,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-es2015-modules-commonjs'],
+        },
       },
-    },
-  ];
-}
-
-module.exports = [
-  {
-    entry: path.join(__dirname, 'src', 'pages', 'popup', 'index.js'),
-    module: {
-      loaders: getLoaders(),
-    },
-    plugins: getPlugins(),
-    output: getOutput('bundle.js'),
+    ],
   },
-  {
-    entry: path.join(__dirname, 'src', 'pages', 'background', 'index.js'),
-    module: {
-      loaders: getLoaders(),
-    },
-    plugins: getPlugins(),
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'background.js',
-    },
-  },
-];
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: [
+        `node ./scripts/build-manifest.js ${targetBrowser} ${distPath}`,
+      ],
+    }),
+  ],
+});
