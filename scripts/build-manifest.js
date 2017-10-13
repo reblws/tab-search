ca564476382f9287e4dae459e93dcb8e5edefe14
Enrico B <rflnrclws@protonmail.com>
2017-10-13 16:28:09 -0400

    Clean up webpack config and add new package.json scripts

--- scripts/build-manifest.js
+++ scripts/build-manifest.js
@@ -1,15 +1,27 @@
+#!/usr/bin/env node
+
+// This script accepts a browser (firefox/chrome) as the first arg and saves the
+// manifest.json file to the dist folder specified in the second arg
+// eslint-disable-next-line
+const { outputJsonSync } = require('fs-extra');
+const { join, resolve } = require('path');
 const baseManifest = require('../src/manifest/base.json');
 
 const targetBrowser = process.argv[2];
-
+const destPath = resolve(join(process.argv[3]), 'manifest.json');
+let manifest;
 switch (targetBrowser) {
   case 'firefox':
-    console.log(Object.assign({}, baseManifest, require('../src/manifest/firefox.json')));
+    manifest = Object.assign({}, baseManifest, require('../src/manifest/firefox.json'));
     break;
   case 'chrome':
-    console.log(Object.assign({}, baseManifest, require('../src/manifest/chrome.json')));
+    manifest = Object.assign({}, baseManifest, require('../src/manifest/chrome.json'));
     break;
   default:
-    return null;
+    throw new Error('Invalid targetBrowser!');
 }
 
+
+outputJsonSync(destPath, manifest, { spaces: 2 });
+console.log('Done writing manifest.json');
+console.log(`Saved in ${destPath}`);
