44beec201260aa2b99eba30106dbc7e86785c0b4
Enrico B <rflnrclws@protonmail.com>
2017-10-16 18:37:19 -0400

    Persist store properly

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,14 @@
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
-import storage from './storage-adapter';
 import rootReducer from '../reducers';
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger),
+);
 
-function configureStore() {
-  let store = createStore(
-    rootReducer,
-    applyMiddleware(thunkMiddleware, logger),
-  );
-  let persistor = persistStore(store);
-  return { persistor, store };
-}
+persistStore(store);
 
 export default store;
