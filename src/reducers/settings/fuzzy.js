808c6c907042598fbe574cf3313a6741fb53c6bb
Enrico B <rflnrclws@protonmail.com>
2017-10-17 19:03:54 -0400

    Handle nested option updates

--- src/reducers/settings/fuzzy.js
+++ src/reducers/settings/fuzzy.js
@@ -6,12 +6,15 @@ const initialState = initialRootState.fuzzySearch;
 export default function fuzzyReducer(state = initialState, action) {
   const type = action.type.split('/')[1];
   const { key, value } = action.payload;
+  if (key === 'enableFuzzySearch') {
+    return Object.assign({}, state, { [key]: value });
+  }
   switch (type) {
     case RANGE_UPDATE:
-      return Object.assign({}, state, { [key]: parseInt(value, 10) });
-    case CHECKBOX_UPDATE:
-      return Object.assign({}, state, { [key]: value });
-    default:
+    case CHECKBOX_UPDATE: {
+      const options = Object.assign({}, state.options, { [key]: value });
+      return Object.assign({}, state, { options });
+    } default:
       return state;
   }
 }
