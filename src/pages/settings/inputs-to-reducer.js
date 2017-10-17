904f24f1b4f19fbaf7a8ce52f70ca4479023b290
Enrico B <rflnrclws@protonmail.com>
2017-10-17 18:58:58 -0400

    Add settings page js, with event listeners and dispatches

---
+++ src/pages/settings/inputs-to-reducer.js
@@ -0,0 +1,15 @@
+/*
+Key mappings from input id names to corresponding reducer state
+*/
+
+export default {
+  showTabCountBadgeText: 'showTabCountBadgeText',
+  showRecentlyClosed: 'showRecentlyClosed',
+  searchAllWindows: 'searchAllWindows',
+  showBookmarks: 'showBookmarks',
+  enableOverlay: 'enableOverlay',
+  enableFuzzySearch: 'fuzzySearch.enableFuzzySearch',
+  shouldSort: 'fuzzySearch.options.shouldSort',
+  threshold: 'fuzzySearch.options.threshold',
+  showUrls: 'fuzzySearch.options.keys', // Modifies 'keys'
+};
