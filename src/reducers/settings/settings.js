57f0f0513e9b312364d342e4b18d6ee7d935abf6
Enrico B <rflnrclws@protonmail.com>
2017-10-17 18:58:21 -0400

    Add fuzzy subreducer and rename types

--- src/reducers/settings/settings.js
+++ src/reducers/settings/settings.js
@@ -1,11 +1,19 @@
-import { UPDATE_SETTING } from '../../actions/types';
+import { CHECKBOX_UPDATE } from '../../actions/types';
 import initialState from './initial-settings';
+import fuzzyReducer from './fuzzy';
 
 export default function settingsReducer(state = initialState, action) {
-  if (action.type !== UPDATE_SETTING) {
+  // Parse the action type
+  const isFuzzy = action.type.split('/')[0] === 'FUZZY';
+
+  if (isFuzzy) {
+    return Object.assign({}, state, { fuzzySearch: fuzzyReducer(action) });
+  }
+
+  if (action.type !== CHECKBOX_UPDATE) {
     return state;
   }
+
   const { key, value } = action.payload;
   return Object.assign({}, state, { [key]: value });
 }
-
