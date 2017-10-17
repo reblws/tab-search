76185fbdb8c4f1c2394142b6e72d7757642ee70d
Enrico B <rflnrclws@protonmail.com>
2017-10-17 19:00:13 -0400

    Add actions for handling setting state in the background

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -1,8 +1,33 @@
-import * as types from './types';
+import {
+  CHECKBOX_UPDATE,
+  RANGE_UPDATE,
+  FUZZY,
+} from './types';
 
-export function updateField(key, value) {
+
+export function updateCheckbox(key, value) {
   return {
-    type: types.SETTING_UPDATE,
+    type: CHECKBOX_UPDATE,
+    payload: {
+      key,
+      value,
+    },
+  };
+}
+
+export function updateFuzzyCheckbox(key, value) {
+  return {
+    type: FUZZY + CHECKBOX_UPDATE,
+    payload: {
+      key,
+      value,
+    },
+  };
+}
+
+export function updateFuzzyRange(key, value) {
+  return {
+    type: FUZZY + RANGE_UPDATE,
     payload: {
       key,
       value,
