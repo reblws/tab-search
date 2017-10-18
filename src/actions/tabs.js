ef162853baf4b9e6bab0ef77858b1cf6b9ddf75c
Enrico B <rflnrclws@protonmail.com>
2017-10-18 19:50:09 -0400

    Add new tab *& window actions for modifying tab state

--- src/actions/tabs.js
+++ src/actions/tabs.js
@@ -4,31 +4,74 @@ import * as types from './types';
 // Loads the active tabs onto the redux store
 export function initializeTabs() {
   return (dispatch) => {
-    // Query for all window-tabs, it's up to each individual window's popup
-    // script to filter the results
-    browser.tabs.query({})
-      .then(tabs => dispatch(receiveTabs(tabs)))
-      .catch(() => {
-        throw new Error('Ran into a problem loading browser\'s tabs.');
+    dispatch({
+      type: types.TABS_INITIALIZE,
+    });
+    const dispatchReceiveTabs = ({ id, tabs }) => {
+      dispatch(receiveWindowTabs(id, tabs));
+    };
+    browser.windows.getAll({ populate: true, windowTypes: ['normal'] })
+      .then((windows) => {
+        windows.forEach(dispatchReceiveTabs);
+      })
+      .catch((e) => {
+        throw new Error(`Ran into a problem loading browser's tabs: ${e}`);
       });
   };
 }
 
-export function deleteTab(tabId) {
+export function createWindow(windowId) {
   return (dispatch) => {
-    browser.tabs.remove(tabId)
-      .catch(() => {
-        throw new Error('Ran into a problem deleting tabs.');
+    dispatch({
+      type: types.WINDOW_CREATE,
+    });
+    browser.windows.get(windowId, { populate: true })
+      .then(({ id, tabs }) => {
+        dispatch(receiveWindowTabs(id, tabs));
+      })
+      .catch((e) => {
+        throw new Error(`Ran into trouble interpreting a created window: ${e}`);
       });
   };
 }
 
-export function receiveTabs(tabs) {
+export function receiveWindowTabs(windowId, tabs) {
   return {
-    type: types.TABS_RECEIVE,
-    payload: tabs,
+    type: types.WINDOW_TABS_RECEIVE,
+    payload: {
+      windowId,
+      tabs,
+    },
   };
 }
+
+export function closeWindow(windowId) {
+  return {
+    type: types.WINDOW_CLOSE,
+    payload: windowId,
+  };
+}
+
+export function deleteTab(tabId, windowId) {
+  return {
+    type: types.TAB_DELETE,
+    payload: {
+      tabId,
+      windowId,
+    },
+  };
+}
+
+export function createTab(tabId, windowId) {
+  return {
+    type: types.TAB_CREATE,
+    payload: {
+      tabId,
+      windowId,
+    },
+  };
+}
+
 
 // Accepts a Tab object and passes the id
 export function updateActiveTab(tab) {
