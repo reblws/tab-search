0bbc398e5b9e3347942ce558ebd7cfd7cc34c5bc
Enrico B <rflnrclws@protonmail.com>
2017-10-18 19:50:52 -0400

    Add background listeners for creating and deleting: windows and tabs

--- src/pages/background/event-listeners.js
+++ src/pages/background/event-listeners.js
@@ -1,17 +1,41 @@
-import { initializeTabs } from '../../actions/tabs';
+import {
+  deleteTab,
+  closeWindow,
+  createWindow,
+  createTab,
+} from '../../actions/tabs';
 
-export default function addBackgroundListeners(dispatch) {
-  const updateTabs = () => {
-    dispatch(initializeTabs());
-  };
-  const events = [
-    browser.tabs.onCreated,
-    browser.tabs.onRemoved,
-    browser.tabs.onReplaced,
-    browser.tabs.onUpdated,
-  ];
-  // Repopulate the tabs object every time a tab is modified.
-  events.forEach((event) => {
-    event.addListener(updateTabs);
-  });
-}
+// export default function addBackgroundListeners(dispatch) {
+//   const updateTabs = () => {
+//     dispatch(initializeTabs());
+//   };
+//   const events = [
+//     browser.tabs.onCreated,
+//     browser.tabs.onRemoved,
+//     browser.tabs.onReplaced,
+//     browser.tabs.onUpdated,
+//   ];
+//   // Repopulate the tabs object every time a tab is modified.
+//   events.forEach((event) => {
+//     // event.addListener(updateTabs);
+//   });
+// }
+
+export const dispatchCloseWindow = dispatch => (windowId) => {
+  dispatch(closeWindow(windowId));
+};
+
+export const dispatchCreateWindow = dispatch => ({ id }) => {
+  dispatch(createWindow(id));
+};
+
+export const dispatchDeleteTab = dispatch => (tabId, removeInfo) => {
+  const { windowId, isWindowClosing } = removeInfo;
+  if (!isWindowClosing) {
+    dispatch(deleteTab(tabId, windowId));
+  }
+};
+
+export const dispatchCreateTab = dispatch => ({ id, windowId }) => {
+  dispatch(createTab(id, windowId));
+};
