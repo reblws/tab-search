31a57a9735f6abe8a71c206275c9d5bec3da4925
Enrico B <rflnrclws@protonmail.com>
2017-10-18 19:50:52 -0400

    Add background listeners for creating and deleting: windows and tabs

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -1,9 +1,22 @@
 import backgroundStore from './store';
-import addBackgroundListeners from './event-listeners';
+import {
+  dispatchCloseWindow,
+  dispatchCreateWindow,
+  dispatchDeleteTab,
+  dispatchCreateTab,
+} from './event-listeners';
 import { initializeTabs } from '../../actions/tabs';
 
 const { dispatch } = backgroundStore;
 
 dispatch(initializeTabs());
-addBackgroundListeners(dispatch);
+// addBackgroundListeners(dispatch);
+browser.windows.onRemoved.addListener(dispatchCloseWindow(dispatch));
+browser.windows.onCreated.addListener(dispatchCreateWindow(dispatch));
+
+browser.tabs.onRemoved.addListener(dispatchDeleteTab(dispatch));
+browser.tabs.onCreated.addListener(dispatchCreateTab(dispatch));
+
+// browser.tabs.onCreated.addListener
+// browser.tabs.onDetached ? browser.tabs.onAttached
 
