619672ffe03706eee1dea4262109dfacfed87906
Enrico B <rflnrclws@protonmail.com>
2017-10-18 19:46:42 -0400

    Only add logger in production

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import { persistStore } from 'redux-persist';
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
+
+const middleware = [thunkMiddleware];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(logger);
+}
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, logger),
+  applyMiddleware(...middleware),
 );
 
 persistStore(store);
