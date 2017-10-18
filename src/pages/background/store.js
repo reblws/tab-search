25adde9eaf35fb393e3bba491399ff1d055afe44
Enrico B <rflnrclws@protonmail.com>
2017-10-18 15:31:11 -0400

    Add ui action keys to background store

--- src/pages/background/store.js
+++ src/pages/background/store.js
@@ -1,4 +1,26 @@
 import { createBackgroundStore } from 'redux-webext';
 import store from '../../store';
+import {
+  CHECKBOX_UPDATE,
+  RANGE_UPDATE,
+  SEARCH_KEY_UPDATE,
+  FUZZY,
+} from '../../actions/types';
+import {
+  updateCheckbox,
+  updateFuzzyCheckbox,
+  updateFuzzyRange,
+  updateFuzzySearchKeys,
+} from '../../actions/settings';
 
-export default createBackgroundStore({ store });
+export default createBackgroundStore({
+  store,
+  actions: {
+    [FUZZY + CHECKBOX_UPDATE]: updateFuzzyCheckbox,
+    [FUZZY + RANGE_UPDATE]: updateFuzzyRange,
+    [CHECKBOX_UPDATE]: updateCheckbox,
+    [FUZZY + SEARCH_KEY_UPDATE]: updateFuzzySearchKeys,
+  },
+});
+
+// actions: { [uiActionType]: [backgroundAction]  }
