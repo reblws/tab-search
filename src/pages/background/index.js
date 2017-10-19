d6f8877d8b8b65925d36f039b8364eaa73387513
Enrico B <rflnrclws@protonmail.com>
2017-10-19 18:56:02 -0400

    Display number of tabs in badge text

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -1,5 +1,38 @@
 import backgroundStore from './store';
+import {
+  startCountingBadgeTextAndAddListeners,
+  stopCountingBadgeTextAndRemoveListeners,
+} from './badge-text-listeners';
 
-const { dispatch } = backgroundStore;
+// Init background
+// First check if we even want to check for badge text, if true then start a
+// timeout to wait for the browser to start up and add call startCounting...
+const { getState } = backgroundStore;
 
-console.log(backgroundStore);
+if (getState().general.showTabCountBadgeText) {
+  const LOADING_TEXT = '...';
+  // Show loading indicator
+  browser.browserAction.setBadgeText({
+    text: LOADING_TEXT,
+  });
+  setTimeout(() => startCountingBadgeTextAndAddListeners(), 1000);
+}
+
+backgroundStore.subscribe(subscribeToBadgeTextState(backgroundStore));
+
+function subscribeToBadgeTextState(store) {
+  let prevValue = store.getState().general.showTabCountBadgeText;
+  return () => {
+    const nextValue = store.getState().general.showTabCountBadgeText;
+    // These should only run if the setting actually changed
+    // If the initial prevValue is already true we dont need to add these
+    // listeners
+    if (nextValue && !prevValue) {
+      startCountingBadgeTextAndAddListeners();
+    } else if (!nextValue && prevValue) {
+      stopCountingBadgeTextAndRemoveListeners();
+    }
+    prevValue = nextValue;
+  };
+}
+
