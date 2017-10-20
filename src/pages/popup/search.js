6ad17e41ed825b7b597ccbad72bc58aa72cf790f
Enrico B <rflnrclws@protonmail.com>
2017-10-20 19:28:57 -0400

    Annotate object array with the object types, and handle specified settings

--- src/pages/popup/search.js
+++ src/pages/popup/search.js
@@ -1,32 +1,67 @@
 import Fuse from 'fuse.js';
+import { deletedTabsCache } from './caches';
+import {
+  TAB_TYPE,
+  OTHER_WINDOW_TAB_TYPE,
+  SESSION_TYPE,
+} from './constants';
+import {
+  identity,
+  isOfWindow,
+  isOfType,
+  annotateTypeCondtionally,
+  annotateType,
+  doubleFilterAndMerge,
+} from './utils/array';
 
-const annotateType = type => tab => Object.assign({}, tab, { type });
-
-export default function filterResults(
+export default function filterResult(
   query,
   options,
   {
     showRecentlyClosed,
     recentlyClosedLimit,
+    alwaysShowRecentlyClosedAtTheBottom,
   },
 ) {
-  return function promiseTabResults(loadedTabs) {
-    const tabsToSearch = loadedTabs.map(annotateType('tab'));
-    return showRecentlyClosed
+  return function promiseSearchResults(loadedTabs) {
+    const isQueryEmpty = query.length === 0;
+    const isTabType = isOfType(TAB_TYPE);
+    const tabFilter = showRecentlyClosed
+      ? identity
+      : ({ id }) => !deletedTabsCache().includes(id);
+    // First filter any unwanted results
+    const annotatedTabs = loadedTabs.filter(tabFilter)
+      .map(
+        annotateTypeCondtionally(
+          isOfWindow(3),
+          [TAB_TYPE, OTHER_WINDOW_TAB_TYPE],
+        ),
+      );
+    // If we want to move the closed tabs to the botttom filter it
+    const shouldMoveClosedToBottom = showRecentlyClosed
+      && alwaysShowRecentlyClosedAtTheBottom;
+    const arrayToSearch = showRecentlyClosed
       ? Promise.all([
-        tabsToSearch,
+        annotatedTabs,
         getRecentlyClosed(recentlyClosedLimit),
       ]).then(([tabs, sessions]) => [...tabs, ...sessions])
-      : Promise.resolve(new Fuse(tabsToSearch, options).search(query));
+      : Promise.resolve(annotatedTabs);
+    const doFinalOperation = shouldMoveClosedToBottom
+      ? doubleFilterAndMerge(isTabType)
+      : identity;
+    const search = isQueryEmpty
+      ? identity
+      : arr => new Fuse(arr, options).search(query);
+    return arrayToSearch
+      .then(search)
+      .then(doFinalOperation);
   };
 }
 
 function getRecentlyClosed(maxResults) {
   const tab = ({ tab: _tab }) => _tab;
+  const getTabsAndAnnotate = objects =>
+    objects.filter(tab).map(tab).map(annotateType(SESSION_TYPE));
   return browser.sessions.getRecentlyClosed({ maxResults })
-    .then(sessionObjects =>
-      sessionObjects.filter(tab).map(tab).map(annotateType('session')),
-    )
-    .then(x => { console.log('sessions', x); return x; });
+    .then(getTabsAndAnnotate);
 }
-
