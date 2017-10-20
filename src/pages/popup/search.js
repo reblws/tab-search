6e84a79ce2a36488ef359994888d23b4dd9d47ed
Enrico B <rflnrclws@protonmail.com>
2017-10-19 21:24:31 -0400

    Query sessions api if recentlyClosedTabs are enabled in settings

--- src/pages/popup/search.js
+++ src/pages/popup/search.js
@@ -1,5 +1,32 @@
 import Fuse from 'fuse.js';
 
-export default function filterResults(query, options) {
-  return tabs => new Fuse(tabs, options).search(query);
+const annotateType = type => tab => Object.assign({}, tab, { type });
+
+export default function filterResults(
+  query,
+  options,
+  {
+    showRecentlyClosed,
+    recentlyClosedLimit,
+  },
+) {
+  return function promiseTabResults(loadedTabs) {
+    const tabsToSearch = loadedTabs.map(annotateType('tab'));
+    return showRecentlyClosed
+      ? Promise.all([
+        tabsToSearch,
+        getRecentlyClosed(recentlyClosedLimit),
+      ]).then(([tabs, sessions]) => [...tabs, ...sessions])
+      : Promise.resolve(new Fuse(tabsToSearch, options).search(query));
+  };
 }
+
+function getRecentlyClosed(maxResults) {
+  const tab = ({ tab: _tab }) => _tab;
+  return browser.sessions.getRecentlyClosed({ maxResults })
+    .then(sessionObjects =>
+      sessionObjects.filter(tab).map(tab).map(annotateType('session')),
+    )
+    .then(x => { console.log('sessions', x); return x; });
+}
+
