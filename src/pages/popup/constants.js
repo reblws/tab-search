675c320fb58fe112cf41d2b205fe6cd0ba2914af
Enrico B <rflnrclws@protonmail.com>
2017-10-20 19:00:06 -0400

    Add (tab, session, other-window) type strings to constants

--- src/pages/popup/constants.js
+++ src/pages/popup/constants.js
@@ -3,3 +3,8 @@ export const alertCircle = '/assets/alert-circle.svg';
 export const deleteButton = document.querySelector('.delete-circle');
 export const searchInput = document.querySelector('.search');
 export const tabList = document.querySelector('.tab-list');
+
+// Object types
+export const TAB_TYPE = 'tab';
+export const SESSION_TYPE = 'session';
+export const OTHER_WINDOW_TAB_TYPE = 'other-window-tab';
