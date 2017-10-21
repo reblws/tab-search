3ecdb7fdded3e7eec6ec1a3334c2960df7bcd722
Enrico B <rflnrclws@protonmail.com>
2017-10-21 18:37:36 -0400

    Add showRecentlyClosedAtBottom key and fix typo

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -2,15 +2,16 @@ export const initialFuzzySettings = {
   enableFuzzySearch: true,
   shouldSort: true,
   threshold: 0.5,
-  maxPattenLength: 32,
+  maxPatternLength: 32,
   minMatchCharLength: 1,
   keys: ['title', 'url'],
 };
 
 export const initialGeneralSettings = {
   showTabCountBadgeText: true,
-  showBookmarks: false,
-  showRecentlyClosed: false,
   searchAllWindows: false,
   enableOverlay: false,
+  showRecentlyClosed: true,
+  alwaysShowRecentlyClosedAtTheBottom: true,
+  recentlyClosedLimit: 5,
 };
