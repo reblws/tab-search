091cc0474b9cd7639337dfbdf8dc27c7c4511d49
Enrico B <rflnrclws@protonmail.com>
2017-10-21 17:27:19 -0400

    Use compose in array utils, and clean up code

--- src/pages/popup/utils/array.js
+++ src/pages/popup/utils/array.js
@@ -1,12 +1,16 @@
+import { compose } from 'redux';
+
 export const identity = x => x;
+
 const isOfProperty =
   propKey => desiredProp => ({ [propKey]: prop }) => prop === desiredProp;
 export const isOfType = isOfProperty('type');
 export const isOfWindow = isOfProperty('windowId');
+
 export const annotateType = type => obj => Object.assign({}, obj, { type });
 
-// Places elements that dont satisfy the predicate to the second index array in
-// the accumlator. Is a reducer.
+// Places elements that dont satisfy the predicate to the second array in
+// the accumlator.
 export const doubleFilterReducer = predicate => ([accepted, rejected], val) => {
   let nextRejected = rejected;
   let nextAccepted = accepted;
@@ -18,29 +22,35 @@ export const doubleFilterReducer = predicate => ([accepted, rejected], val) => {
   return [nextAccepted, nextRejected];
 };
 
-export const doubleFilterArray =
-  predicate => arr => arr.reduce(doubleFilterReducer(predicate), [[], []]);
+export const doubleFilterArray = predicate => array => array.reduce(
+  doubleFilterReducer(predicate),
+  [[], []],
+);
+
+const concat = (acc, arr) => [].concat(acc, arr);
+const mergeArray = array => array.reduce(concat);
 
 // Moves elements that dont satisfy the predicate to the tail of the array in-order
-export const doubleFilterAndMerge =
-  predicate => arr => [].concat(...doubleFilterArray(predicate)(arr));
+export const doubleFilterAndMerge = predicate => compose(
+  mergeArray,
+  doubleFilterArray(predicate),
+);
 
-export const annotateTypeConditionally = (
-  predicate,
-  [acceptedType, rejectedType],
-) => (obj) => {
-  const annotateAcceptedType = getAnnotationFunction(acceptedType, obj);
-  const annotateRejectedType = getAnnotationFunction(rejectedType, obj);
-  return predicate(obj)
-    ? annotateAcceptedType(obj)
-    : annotateRejectedType(obj);
-};
-
-  // If given a type as a function, apply that function to the object to get its
-  // type
+// If given a type as a function, apply that function to the object to get its
+// type
 export const getAnnotationFunction = (typeToAnnotate, obj) => {
   if (typeof typeToAnnotate === 'function') {
     return annotateType(typeToAnnotate(obj));
   }
   return annotateType(typeToAnnotate);
 };
+
+export const annotateTypeConditionally = (
+  predicate,
+  acceptedType,
+  rejectedType,
+) => obj => getAnnotationFunction(
+  predicate(obj) ? acceptedType : rejectedType,
+  obj,
+)(obj);
+
