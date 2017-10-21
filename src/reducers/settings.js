ca394fab2619db3c15c5a73cfe1c015d747b5c75
Enrico B <rflnrclws@protonmail.com>
2017-10-21 19:58:43 -0400

    Add NUMBER_UPDATE action

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,4 +1,4 @@
-import { CHECKBOX_UPDATE, SETTINGS_RESET } from '../actions/types';
+import { CHECKBOX_UPDATE, SETTINGS_RESET, NUMBER_UPDATE } from '../actions/types';
 import { initialGeneralSettings } from './defaults';
 
 export default function generalSettingsReducer(state = initialGeneralSettings, action) {
@@ -11,6 +11,10 @@ export default function generalSettingsReducer(state = initialGeneralSettings, a
     }
     case SETTINGS_RESET: {
       return initialGeneralSettings;
+    }
+    case NUMBER_UPDATE: {
+      const { key, value } = action.payload;
+      return Object.assign({}, state, { [key]: parseInt(value, 10) });
     }
     default:
       return state;
