fab68729d9109421b40237143081441b38a7b761
Enrico B <rflnrclws@protonmail.com>
2017-10-25 20:38:12 -0400

    Fix storage adapter not serializing/deserializing stored state properly
    getItem has its JSON.parse op removed, goes straight to the key specified
    
    setItem parses each key's value and returns that new plain object instead

--- src/store/storage-adapter.js
+++ src/store/storage-adapter.js
@@ -2,7 +2,21 @@
 const storage = browser.storage.local;
 
 export default {
-  getItem: key => storage.get(key).then(JSON.parse),
-  setItem: (key, item) => storage.set({ [key]: JSON.stringify(item) }),
+  getItem: key => storage.get(key).then(obj => obj[key]),
+  setItem: (key, item) => {
+    // Problem with original is that once we parse the stringified state
+    // given by redux-persist, all the object-values are still stringified.
+
+    // storage.local stores native JS values, all we do here is convert the
+    // stringified state back into a POJO
+
+    // This is a quick fix, only works 2 levels deep
+    const stringifiedState = JSON.parse(item); // All its values are strings
+    const parsedValues = Object.values(stringifiedState).map(JSON.parse);
+    const parsedItem = Object.keys(stringifiedState)
+      .map((k, index) => ({ [k]: parsedValues[index] }))
+      .reduce((acc, val) => Object.assign({}, acc, val));
+    return storage.set({ [key]: parsedItem });
+  },
   removeItem: key => storage.remove(key),
 };
