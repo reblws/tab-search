967c8b928c25112c38c5afeb3b95316db892b1a6
Enrico B <rflnrclws@protonmail.com>
2017-10-26 00:34:16 -0400

    Fix issue where badge was showing on browser startup even after disabledit was

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -7,21 +7,18 @@ import {
 // Init background
 // First check if we even want to check for badge text, if true then start a
 // timeout to wait for the browser to start up and add call startCounting...
-const { getState } = backgroundStore;
-
-if (getState().general.showTabCountBadgeText) {
-  const LOADING_TEXT = '...';
-  // Show loading indicator
-  browser.browserAction.setBadgeText({
-    text: LOADING_TEXT,
-  });
-  setTimeout(() => startCountingBadgeTextAndAddListeners(), 1000);
-}
-
 backgroundStore.subscribe(subscribeToBadgeTextState(backgroundStore));
 
 function subscribeToBadgeTextState(store) {
   let prevValue = store.getState().general.showTabCountBadgeText;
+  if (prevValue) {
+    const LOADING_TEXT = '...';
+    // Show loading indicator
+    browser.browserAction.setBadgeText({
+      text: LOADING_TEXT,
+    });
+    setTimeout(() => startCountingBadgeTextAndAddListeners(), 1000);
+  }
   return () => {
     const nextValue = store.getState().general.showTabCountBadgeText;
     // These should only run if the setting actually changed
