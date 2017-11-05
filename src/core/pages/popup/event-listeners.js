a6065db0a28e0e84097b769646f8097061e50291
Enrico B <reblws@protonmail.com>
2017-11-05 17:22:50 -0500

    #13: Fix input not focusing by reinstating the focus timeout

--- src/core/pages/popup/event-listeners.js
+++ src/core/pages/popup/event-listeners.js
@@ -14,14 +14,17 @@ import {
 } from './event-callbacks';
 
 export function addEventListeners(store) {
-  // Populate the here and now
   const updateSearchResults = configureSearch(store);
   const handleKeydown = keydownHandler(store);
-  populateTabList(updateSearchResults()).then(() => searchInput.focus());
   window.addEventListener('keydown', handleKeydown);
   deleteButton.addEventListener('click', clearInput);
   searchInput.addEventListener('change', updateSearchResults);
   searchInput.addEventListener('keyup', updateSearchResults);
+  // Populate the initial tab list here.
+  // Set a timeout so the input actually focuses
+  // TODO: Add option for showing last query on popup
+  populateTabList(updateSearchResults())
+    .then(() => setTimeout(() => searchInput.focus(), 150));
   return store;
 }
 
