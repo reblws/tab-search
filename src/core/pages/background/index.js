50aac7cc8fe03c9408c08cce498bac73105cbcea
Enrico B <reblws@protonmail.com>
2017-11-14 13:31:56 -0500

    Clear badge text and set it when enabling or disabling badge text option

--- src/core/pages/background/index.js
+++ src/core/pages/background/index.js
@@ -11,8 +11,8 @@ backgroundStore.subscribe(subscribeToBadgeTextState(backgroundStore));
 
 function subscribeToBadgeTextState(store) {
   let prevValue = store.getState().general.showTabCountBadgeText;
+  const LOADING_TEXT = '...';
   if (prevValue) {
-    const LOADING_TEXT = '...';
     // Show loading indicator
     browser.browserAction.setBadgeText({
       text: LOADING_TEXT,
@@ -25,8 +25,10 @@ function subscribeToBadgeTextState(store) {
     // If the initial prevValue is already true we dont need to add these
     // listeners
     if (nextValue && !prevValue) {
+      browser.browserAction.setBadgeText({ text: LOADING_TEXT });
       startCountingBadgeTextAndAddListeners();
     } else if (!nextValue && prevValue) {
+      browser.browserAction.setBadgeText({ text: '' });
       stopCountingBadgeTextAndRemoveListeners();
     }
     prevValue = nextValue;
