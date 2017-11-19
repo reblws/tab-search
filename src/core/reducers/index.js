2b4a4d4060ed2f1b93e6f6d3da740a180a65147e
Enrico B <reblws@protonmail.com>
2017-11-19 14:57:06 -0500

    Update redux-persist to v5.4.0, disable state serialization

--- src/core/reducers/index.js
+++ src/core/reducers/index.js
@@ -3,12 +3,11 @@ import { persistReducer } from 'redux-persist';
 import fuzzySettingsReducer from './fuzzy-settings';
 import generalSettingsReducer from './settings';
 import storage from './services/storage-adapter';
-import getStoredState from './services/get-stored-state';
 
 const persistConfig = {
   key: 'root',
   storage,
-  getStoredState,
+  serialize: false,
 };
 
 
