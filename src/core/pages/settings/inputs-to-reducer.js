9f763cd606876c6a65a3953b50c4f45cb37b8f60
Enrico B <reblws@protonmail.com>
2017-11-19 15:47:27 -0500

    Get reducer paths programmatically

--- src/core/pages/settings/inputs-to-reducer.js
+++ src/core/pages/settings/inputs-to-reducer.js
@@ -1,19 +1,66 @@
 /*
-Key mappings from input id names to corresponding reducer state
+Key mappings from input id names to corresponding reducer state.
+This works as long as these two reducers are only a flat 1-level object
 */
-// TODO: there's probably a way to do this programmatically instead of relying
-//       on literals. Need to import '/reducers/defaults'
-export default {
-  showTabCountBadgeText: 'general.showTabCountBadgeText',
-  showRecentlyClosed: 'general.showRecentlyClosed',
-  searchAllWindows: 'general.searchAllWindows',
-  alwaysShowRecentlyClosedAtTheBottom: 'general.alwaysShowRecentlyClosedAtTheBottom',
-  recentlyClosedLimit: 'general.recentlyClosedLimit',
-  enableOverlay: 'general.enableOverlay',
-  useFallbackFont: 'general.useFallbackFont',
-  enableFuzzySearch: 'fuzzy.enableFuzzySearch',
-  shouldSort: 'fuzzy.shouldSort',
-  threshold: 'fuzzy.threshold',
-  showUrls: 'fuzzy.keys', // Modifies 'keys'
+import {
+  initialFuzzySettings,
+  initialGeneralSettings,
+} from 'core/reducers/defaults';
+
+
+const settingSkeleton = {
+  fuzzy: Object.keys(initialFuzzySettings),
+  general: Object.keys(initialGeneralSettings),
 };
 
+/*
+1. Map (x -> 'fuzzy.' + x)
+
+[
+  enableFuzzySearch,
+  shouldSort,
+  threshold,
+  maxPatternLength,
+  minMatchCharLength,
+  keys
+]
+*/
+
+const skeleton = {
+  fuzzy: Object.keys(initialFuzzySettings),
+  general: Object.keys(initialGeneralSettings),
+};
+
+export default Object.keys(skeleton).reduce((acc, keyPrefix) => {
+  const values = skeleton[keyPrefix];
+  return Object.assign(
+    {},
+    acc,
+    values.reduce((settingAcc, setting) => {
+      // In the form showUrls is in charge of changing the 'keys' prop in reducer
+      const settingKey = (setting !== 'keys')
+        ? setting
+        : 'showUrls';
+      return Object.assign(
+        {},
+        settingAcc,
+        { [settingKey]: `${keyPrefix}.${setting}` },
+      );
+    }, {}),
+  );
+}, {});
+
+// export default {
+//   showLastQueryOnPopup: 'general.showLastQueryOnPopup',
+//   showTabCountBadgeText: 'general.showTabCountBadgeText',
+//   showRecentlyClosed: 'general.showRecentlyClosed',
+//   searchAllWindows: 'general.searchAllWindows',
+//   alwaysShowRecentlyClosedAtTheBottom: 'general.alwaysShowRecentlyClosedAtTheBottom',
+//   recentlyClosedLimit: 'general.recentlyClosedLimit',
+//   enableOverlay: 'general.enableOverlay',
+//   useFallbackFont: 'general.useFallbackFont',
+//   enableFuzzySearch: 'fuzzy.enableFuzzySearch',
+//   shouldSort: 'fuzzy.shouldSort',
+//   threshold: 'fuzzy.threshold',
+//   showUrls: 'fuzzy.keys', // Modifies 'keys'
+// };
