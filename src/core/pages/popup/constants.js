982294967ec2c1a17d225d0b89fee90d7595327c
Enrico B <reblws@protonmail.com>
2017-11-21 17:28:42 -0500

    Add bookmark svg and path (#20)

--- src/core/pages/popup/constants.js
+++ src/core/pages/popup/constants.js
@@ -1,5 +1,6 @@
 export const favIconFallback = '/assets/file.svg';
 export const alertCircle = '/assets/alert-circle.svg';
+export const BOOKMARKS_SVG_PATH = '/assets/bookmark.svg';
 export const deleteButton = document.querySelector('.delete-circle');
 export const searchInput = document.querySelector('.search');
 export const tabList = document.querySelector('.tab-list');
