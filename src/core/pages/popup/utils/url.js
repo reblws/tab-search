9d3483361eec74c0542d176dfb0ccb1c066de479
Enrico B <reblws@protonmail.com>
2017-11-24 18:08:00 -0500

    Add url utils

---
+++ src/core/pages/popup/utils/url.js
@@ -0,0 +1,12 @@
+// Helpers for handling URLs
+export function encodeUrl(url) {
+    return encodeURI(url);
+}
+
+export function decodeUrl(url) {
+    return decodeURI(url);
+}
+
+export function parseUrl(url) {
+    return new URL(url);
+}
