30e283d62d2e33bd89d09e2b16b014ea0fc8d579
Enrico B <reblws@protonmail.com>
2017-11-28 17:25:17 -0500

    Add default keybindings

--- src/core/keyboard/defaults.js
+++ src/core/keyboard/defaults.js
@@ -1,26 +1,16 @@
 import * as c from './constants';
+import { kbdCommand } from './constructor';
+
 export default {
-  [c.TAB_DELETE]: {
-
-  },
-  [c.TAB_OPEN]:
-  [c.TAB_NEXT]:
-  [c.TAB_PREV]:
-  [c.TAB_MOVE]:
-  [c.TAB_REFRESH]:
-  [c.TAB_PIN]:
-  [c.URL_COPY]:
-  [c.TAB_BOOKMARK]:
-  [c.DUPLICATE_TAB_DELETE]:
-  [c.MUTE_TOGGLE]:
-}
-
-
-
-
-
-
-
-
-
-
+  [c.TAB_DELETE]: kbdCommand('Ctrl+Backspace'),
+  [c.TAB_OPEN]: kbdCommand('Enter'),
+  [c.TAB_NEXT]: kbdCommand('ArrowDown'),
+  [c.TAB_PREV]: kbdCommand('ArrowUp'),
+  // [c.TAB_MOVE]: kbdCommand('')
+  [c.TAB_REFRESH]: kbdCommand('Alt+R'),
+  [c.TAB_PIN]: kbdCommand('Alt+P'),
+  [c.URL_COPY]: kbdCommand('Alt+C'),
+  [c.TAB_BOOKMARK]: kbdCommand('Alt+B'),
+  [c.DUPLICATE_TAB_DELETE]: kbdCommand('Alt+Shift+D'),
+  [c.MUTE_TOGGLE]: kbdCommand('Alt+M'),
+};
