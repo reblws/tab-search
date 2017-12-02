a222eaebb1b1464ecdaad36194ff27ff57d784a8
Enrico B <reblws@protonmail.com>
2017-12-01 20:11:19 -0500

    Add partition function

--- src/core/pages/popup/utils/array.js
+++ src/core/pages/popup/utils/array.js
@@ -1,40 +1,37 @@
-import { compose } from 'redux';
-
 export const identity = x => x;
 
 const isOfProperty =
   propKey => desiredProp => ({ [propKey]: prop }) => prop === desiredProp;
 export const isOfType = isOfProperty('type');
 export const isOfWindow = isOfProperty('windowId');
+export const isActive = isOfProperty('active')(true);
 
 export const annotateType = type => obj => Object.assign({}, obj, { type });
+// Given a variable number of predicates, returns an array of arrays length
 
-// Places elements that dont satisfy the predicate to the second array in
-// the accumlator.
-export const doubleFilterReducer = predicate => ([accepted, rejected], val) => {
-  let nextRejected = rejected;
-  let nextAccepted = accepted;
-  if (predicate(val)) {
-    nextAccepted = [...nextAccepted, val];
-  } else {
-    nextRejected = [...nextRejected, val];
-  }
-  return [nextAccepted, nextRejected];
+// (length of predicates + 1).
+// If element satisfies predicate -> place it in that predicate's index
+// If no predicates satisfied -> place it in (length of predicates) index
+export const partition = (...predicates) => (array) => {
+  const initialPartitions = [...predicates.map(() => []), []];
+  const partitionIndices = predicates.reduce(
+    (acc, p, index) => acc.set(p, index),
+    new Map().set(undefined, predicates.length),
+  );
+  return array.reduce((acc, x) => {
+    const pred = predicates.find(p => p(x));
+    const partitionIndex = partitionIndices.get(pred)
+    return Object.assign(
+      [],
+      acc,
+      { [partitionIndex]: [...acc[partitionIndex], x] },
+    );
+  }, initialPartitions);
 };
 
-export const doubleFilterArray = predicate => array => array.reduce(
-  doubleFilterReducer(predicate),
-  [[], []],
-);
-
-const concat = (acc, arr) => [].concat(acc, arr);
-const mergeArray = array => array.reduce(concat);
+export const concat = (acc, arr) => [].concat(acc, arr);
 
 // Moves elements that dont satisfy the predicate to the tail of the array in-order
-export const doubleFilterAndMerge = predicate => compose(
-  mergeArray,
-  doubleFilterArray(predicate),
-);
 
 // If given a type as a function, apply that function to the object to get its
 // type
