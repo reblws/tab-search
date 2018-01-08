74a1c15853b76275696aa46c2dd350183d75ba52
Enrico B <reblws@protonmail.com>
2018-01-08 14:34:29 -0500

    Add kbdCommandToString for string representation of kbdCommand

---
+++ src/core/keyboard/to-string.js
@@ -0,0 +1,18 @@
+// Returns a string representation of a kbdCommand
+// TODO: Need to get user's OS. If they are on a mac, replace instances of
+//       ctrlKey with 'Cmd'.
+export function kbdCommandToString({ key, ctrlKey, altKey, shiftKey }) {
+  const stringParts = [];
+  if (ctrlKey) {
+    // TODO: If user is on mac, change this to Cmd.
+    stringParts.push('Ctrl');
+  }
+  if (shiftKey) {
+    stringParts.push('Shift');
+  }
+  if (altKey) {
+    stringParts.push('Alt');
+  }
+  stringParts.push(key);
+  return stringParts.join('+');
+}
