0d6f5c10c21fe3709840187ff8a183fcd57126fe
Enrico B <reblws@protonmail.com>
2018-01-10 22:47:57 -0500

    Simplify keyboard interface

---
+++ src/core/keyboard/index.js
@@ -0,0 +1,4 @@
+export * from './constants';
+export { compareKbdCommand } from './compare';
+export { defaultCommands } from './defaults';
+export { isValidKbdCommand, kbdCommand } from './constructor';
