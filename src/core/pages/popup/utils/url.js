dc786601e95390ab07872d69c585ed2112170580
Enrico B <reblws@protonmail.com>
2018-01-15 14:27:01 -0500

    Add scheme validation for bookmarks

--- src/core/pages/popup/utils/url.js
+++ src/core/pages/popup/utils/url.js
@@ -10,3 +10,22 @@ export function decodeUrl(url) {
 export function parseUrl(url) {
   return new URL(url);
 }
+
+// Valid URI schemes for bookmarks
+const VALID_SCHEMES = [
+  'about',
+  'ftp',
+  'mailto',
+  'file',
+  'data',
+  'irc',
+].map(s => `${s}:`);
+
+// Returns whether this url has a hostname or scheme that lets us travel to it
+// like a regular tab
+export function hasValidHostname(url) {
+  if (!url.hostname) {
+    return VALID_SCHEMES.includes(url.protocol);
+  }
+  return !!url.hostname;
+}
