e4cae8afebd2d46e82e7871469adee17885cbc98
Enrico B <reblws@protonmail.com>
2018-01-14 20:21:51 -0500

    Name defaultCommands import

--- src/core/keyboard/index.js
+++ src/core/keyboard/index.js
@@ -1,14 +1,13 @@
-import { defaultCommands } from './defaults';
 import { compareKbdCommand } from './compare';
 import { isValidKbdCommand, kbdCommand } from './constructor';
 import { kbdCommandToString } from './to-string';
 
+export { defaultCommands } from './defaults';
 export * from './constants';
 
 export default {
   isEqual: compareKbdCommand,
   isValid: isValidKbdCommand,
   toString: kbdCommandToString,
-  defaultCommands,
   command: kbdCommand,
 };
