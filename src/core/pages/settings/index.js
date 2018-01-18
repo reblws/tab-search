af88966a1c74494696feb4518d6e9c7a2dee6a8c
Enrico B <reblws@protonmail.com>
2018-01-18 18:10:16 -0500

    Add version string to settings page

--- src/core/pages/settings/index.js
+++ src/core/pages/settings/index.js
@@ -1,9 +1,12 @@
 import { createUIStore } from 'redux-webext';
-import { resetSettings } from './actions';
 import addInputBindings from './input-bindings';
 import { initKeybindingTable, initSettings } from './dom';
 
 addInputBindings();
+
+// Add version info
+document.getElementById('versioning').textContent =
+  `v${browser.runtime.getManifest().version}`;
 
 const appendOsToStore = store =>
   browser.runtime.getPlatformInfo().then(({ os }) => Object.assign({ os }, store));
@@ -13,10 +16,6 @@ createUIStore()
   .then((store) => {
     initSettings(store);
     initKeybindingTable(store);
-    document.getElementById('reset-defaults').addEventListener('click', () => {
-      store.dispatch(resetSettings());
-      location.reload(true);
-    });
     return store;
   }).catch((e) => {
     throw e;
