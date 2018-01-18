363b33574151da7d9706829826489d2840f4d7b8
Enrico B <reblws@protonmail.com>
2018-01-18 18:23:55 -0500

    Don't print final key if it's a modifier

--- src/core/keyboard/to-string.js
+++ src/core/keyboard/to-string.js
@@ -1,6 +1,15 @@
+import {
+  SHIFT,
+  ALT,
+  CONTROL,
+  CTRL,
+  META,
+} from './constants';
+
+const isKeyModifier = m =>
+  m === CONTROL || m === ALT || m === META || m === CTRL || m === SHIFT;
+
 // Returns a string representation of a kbdCommand
-// TODO: Need to get user's OS. If they are on a mac, replace instances of
-//       ctrlKey with 'Cmd'.
 export function kbdCommandToString(input, isMac = false) {
   if (typeof input === 'string') {
     return input;
@@ -20,6 +29,8 @@ export function kbdCommandToString(input, isMac = false) {
   if (altKey) {
     stringParts.push('Alt');
   }
-  stringParts.push(key.length === 1 ? key.toUpperCase() : key);
+  if (!isKeyModifier(key)) {
+    stringParts.push(key.length === 1 ? key.toUpperCase() : key);
+  }
   return stringParts.join(' + ');
 }
