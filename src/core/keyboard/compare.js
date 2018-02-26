efe31ffd074aa24cb6d92fdb265fdcb7bbccbc30
Enrico B <reblws@protonmail.com>
2018-02-26 18:09:37 -0500

    Handle array case in keyboard command comparison

--- src/core/keyboard/compare.js
+++ src/core/keyboard/compare.js
@@ -2,5 +2,8 @@ import dEqual from 'fast-deep-equal';
 
 // Compares two possible objects
 export function compareKbdCommand(c1, c2) {
+  if (Array.isArray(c1)) {
+    return dEqual(c1[0], c1[1]);
+  }
   return dEqual(c1, c2);
 }
