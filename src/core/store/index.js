90495a6f573f8e5ca319776fda0448223aca146c
Enrico B <reblws@protonmail.com>
2018-02-26 14:03:25 -0500

    Remove redux-thunk

--- src/core/store/index.js
+++ src/core/store/index.js
@@ -1,10 +1,9 @@
 import { persistStore } from 'redux-persist';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const middleware = [thunkMiddleware, logger];
+const middleware = [logger];
 const store = createStore(
   rootReducer,
   applyMiddleware(...middleware),
