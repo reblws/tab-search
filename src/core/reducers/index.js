6a7b6c66eb43c889440e88b5cb086aa358b4fa8e
Enrico B <reblws@protonmail.com>
2018-02-27 18:04:37 -0500

    Add migrations to 0.4.6 store

--- src/core/reducers/index.js
+++ src/core/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
+import { persistReducer, createMigrate } from 'redux-persist';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import fuzzySettingsReducer from './fuzzy-settings';
 import generalSettingsReducer from './settings';
@@ -7,6 +7,7 @@ import keyboardConfigReducer from './keyboard';
 import colorSettingsReducer from './colors';
 import stateReducer from './state';
 import storage from './services/storage-adapter';
+import migrations from './services/migrations';
 
 const persistConfig = {
   key: 'root',
@@ -14,9 +15,10 @@ const persistConfig = {
   serialize: false,
   blacklist: ['state'],
   stateReconciler: autoMergeLevel2,
-  // Unversioned configs are set to -1 by default
-  // TODO: Write migrations
   version: 0,
+  migrate: createMigrate(migrations, {
+    debug: process.env.NODE_ENV === 'development',
+  }),
 };
 
 
